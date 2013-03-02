9e6880f0c46fb0b239345575b03bb216369ec7d9
Jorick Caberio <circuitfreakcoder@gmail.com>
2013-03-02 22:16:41 +0800

    C# Windows Forms Application
    C# Windows Forms Application that sends SMS using AT Commands

---
+++ CSharp_SMS/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CSharp_SMS
+{
+    partial class Form_SMS_Sender
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.labelNumber = new System.Windows.Forms.Label();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(90, 44);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(169, 20);
+            this.textBoxNumber.TabIndex = 0;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(90, 101);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(169, 89);
+            this.textBoxMessage.TabIndex = 1;
+            // 
+            // labelNumber
+            // 
+            this.labelNumber.AutoSize = true;
+            this.labelNumber.Location = new System.Drawing.Point(29, 47);
+            this.labelNumber.Name = "labelNumber";
+            this.labelNumber.Size = new System.Drawing.Size(44, 13);
+            this.labelNumber.TabIndex = 2;
+            this.labelNumber.Text = "Number";
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(32, 101);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(50, 13);
+            this.labelMessage.TabIndex = 3;
+            this.labelMessage.Text = "Message";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(90, 209);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 4;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(29, 257);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(74, 13);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "Status: Ready";
+            // 
+            // Form_SMS_Sender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 279);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.labelNumber);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.textBoxNumber);
+            this.Name = "Form_SMS_Sender";
+            this.Text = "SMS Sender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label labelNumber;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ CSharp_SMS/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_SMS
+{
+    public partial class Form_SMS_Sender : Form
+    {
+        private SerialPort _serialPort;
+        public Form_SMS_Sender()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string number = textBoxNumber.Text;
+            string message = textBoxMessage.Text;
+
+            _serialPort = new SerialPort("COM7", 115200);   //Replace "COM7" with corresponding port name
+
+            Thread.Sleep(1000);
+
+            _serialPort.Open();
+
+            Thread.Sleep(1000);
+
+            _serialPort.Write("AT+CMGF=1\r");
+
+            Thread.Sleep(1000);
+
+            _serialPort.Write("AT+CMGS=\"" + number + "\"\r\n");
+
+            Thread.Sleep(1000);
+
+            _serialPort.Write(message + "\x1A");
+
+            Thread.Sleep(1000);
+
+            labelStatus.Text = "Status: Message sent";
+
+            _serialPort.Close();
+        }
+    }
+}
